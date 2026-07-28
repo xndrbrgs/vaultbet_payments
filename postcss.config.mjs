a3f764e8a29d20acb7b6c0ef926baf13620a4a9d
xndrbrgs <aborgescolon@gmail.com>
2026-07-27 21:23:02 -0400

    init

--- postcss.config.mjs
+++ postcss.config.mjs
@@ -1,7 +1,7 @@
+import clampwind from "postcss-clampwind";
+
 const config = {
-  plugins: {
-    "@tailwindcss/postcss": {},
-  },
+  plugins: ["@tailwindcss/postcss", clampwind()],
 };
 
 export default config;
